4072d332a067e168072b241ee22ff66b7f0dfacf
Daniel McGrath <danmcgrath1035@gmail.com>
2019-03-16 16:31:00 -0400

    added some Button Stuff

--- app/controllers/homeScreen/homeScreen.tsx
+++ app/controllers/homeScreen/homeScreen.tsx
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
 
 export default class HomeScreen extends Component {
     render() {
         return(
-            <View style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}>
-                <Text>
-                    Hello World!!!
-                </Text>
+            <View style={styles.container}>
+                <View style={styles.mapContainer}>
+                    <Text style={{justifyContent: 'center'}}>
+                        Insert Map in this view
+                    </Text>
+                </View>
+                <View style={styles.footerContainer}>
+                    <TouchableOpacity style={styles.footerButton}>
+                        <View >
+                            <Text>
+                                blah
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.footerButton}>
+                        <View >
+                            <Text>
+                                blah
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.footerButton}>
+                        <View >
+                            <Text>
+                                blah
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    mapContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    map: {
+
+    },
+    footerContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        maxHeight: 50,
+        
+    },
+    footerButton: {
+        borderColor: '#898989',
+        borderWidth: 1,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
