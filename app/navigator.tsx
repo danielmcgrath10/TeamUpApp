5f3ecac038b97a1087cc4859e8834f776b828ce8
Daniel McGrath <danmcgrath1035@gmail.com>
2019-03-18 20:51:47 -0400

    made some changes

--- app/navigator.tsx
+++ app/navigator.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import HomeScreen from './controllers/homeScreen';
 import AddGroup from './controllers/addGroupController';
+import {HomeIcon} from './shared/images/Icons/Home/1x';
 import {createAppContainer, createBottomTabNavigator} from 'react-navigation';
 
 const TabNavigator = createBottomTabNavigator({
   Home: {screen: HomeScreen},
   AddGroup: {screen: AddGroup}
 }, {
-    initialRouteName: 'Home',
+    initialRouteName: 'Home'
 });
 export default createAppContainer(TabNavigator);
