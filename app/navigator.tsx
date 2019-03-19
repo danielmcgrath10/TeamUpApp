62d3b4dfb8c7edeb195df10b1055283ec4dcf735
Daniel McGrath <danmcgrath1035@gmail.com>
2019-03-18 20:52:15 -0400

    changed

--- app/navigator.tsx
+++ app/navigator.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import HomeScreen from './controllers/homeScreen';
 import AddGroup from './controllers/addGroupController';
-import {HomeIcon} from './shared/images/Icons/Home/1x';
 import {createAppContainer, createBottomTabNavigator} from 'react-navigation';
 
 const TabNavigator = createBottomTabNavigator({
