df6411135c1db31558cdd951037a19dec2f754aa
danielmcgrath10 <39033419+danielmcgrath10@users.noreply.github.com>
2019-03-20 23:18:28 -0400

    working icons

--- app/navigator.tsx
+++ app/navigator.tsx
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import HomeScreen from './controllers/homeScreen';
 import AddGroup from './controllers/addGroupController';
 import {createAppContainer, createBottomTabNavigator} from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const TabNavigator = createBottomTabNavigator({
-  Home: {screen: HomeScreen},
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-home" size={24} />      
+      )
+    }
+  },
   AddGroup: {screen: AddGroup}
 }, {
     initialRouteName: 'Home'
