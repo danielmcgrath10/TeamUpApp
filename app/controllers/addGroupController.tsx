d47e9b7ad149e7fcc41a7c6ef80874ab5ca36db3
danielmcgrath10 <39033419+danielmcgrath10@users.noreply.github.com>
2019-03-21 12:20:47 -0400

    Working Stuff

--- app/controllers/addGroupController.tsx
+++ app/controllers/addGroupController.tsx
@@ -1,15 +1,123 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Picker, PickerItem, StyleSheet, Button, TouchableOpacity} from 'react-native';
 
-export default class AddGroup extends Component {
+
+export default class AddGroup extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state.SportChoice = 'Default Value';
+    // }
     render() {
         return(
-            <View>
-                <Text>
-                    What is Love
-                </Text>
+            <View style={styles.container}>
+                <View style={styles.picker}>
+                    <Picker
+                        selectedValue = 'Choose a Sport'
+                        onValueChange={(PickerItem) => {
+                            //this.setState(this.props.SportChoice, PickerItem.label)
+                            console.log('You chose:' + PickerItem.label)
+                        }}
+                    >
+                        <PickerItem label='Choose a Sport' value='Default Value' />
+                        <PickerItem label="hey" value='pick1'/>
+                        <PickerItem label="there" value='pick2'/>
+                        <PickerItem label="judge" value='pick3'/>
+                    </Picker>
+                </View>
+                
+                <View style={styles.picker}>
+                    <Picker
+                        selectedValue = "Choose a Difficulty"
+                    >
+                         <PickerItem label='Choose a Difficulty' value='Default Value' />
+                        <PickerItem label="hey" value='pick1'/>
+                        <PickerItem label="there" value='pick2'/>
+                        <PickerItem label="judge" value='pick3'/>
+                    </Picker>
+                </View>
+
+                <View style={styles.picker}>
+                    <Picker
+                        selectedValue = "Choose Number of People Needed"
+                    >
+                        <PickerItem label='Choose Number of People Needed' value='Default Value' />
+                        <PickerItem label="hey" value='pick1'/>
+                        <PickerItem label="there" value='pick2'/>
+                        <PickerItem label="judge" value='pick3'/>
+                    </Picker>
+                </View>
+
+                <View style={styles.textBox}>
+                    <Text>
+                        Please Input a Location:
+                    </Text>
+                </View>
+                <View style={styles.ButtonContainer}>
+                    
+                       <TouchableOpacity
+                            style = {{flex: 1}}
+                            onPress={() => {
+                                console.log("you pressed use current")
+                            }}
+                       >
+                            <View style={styles.ButtonStyles}>
+                                <Text>
+                                    Use Current
+                                </Text>
+                            </View>
+                       </TouchableOpacity>
+
+                    <TouchableOpacity 
+                        style={{flex:1}}
+                        onPress={()=>{
+                        console.log('You Pressed Choose from map')
+                        }}
+                    >
+                        <View style={styles.ButtonStyles}>
+                            <Text>
+                                Choose From a Map
+                            </Text>
+                        </View>  
+                    </TouchableOpacity>    
+                </View>
+
             </View> 
         )
     }
     
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-start'
+    },
+    picker: {
+        flex: 1,
+        justifyContent: 'center',
+        borderColor: 'lightgrey',
+        borderBottomWidth: 1,
+        maxHeight: 60,
+    },
+    textBox: {
+        flex: 1,
+        justifyContent: 'center',
+        maxHeight: 50,
+        paddingLeft: 10
+    },
+    ButtonContainer: {
+        flex: 2,
+        flexDirection: 'row',
+        maxHeight: 40,
+        justifyContent: 'space-evenly'
+    },
+    ButtonStyles: {
+        padding: 10,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: 'lightgrey',
+        borderWidth: 0.5
+    }
+})
--- app/navigator.tsx
+++ app/navigator.tsx
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import HomeScreen from './controllers/homeScreen';
 import AddGroup from './controllers/addGroupController';
+import SearchScreen from './controllers/searchController';
 import {createAppContainer, createBottomTabNavigator} from 'react-navigation';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const TabNavigator = createBottomTabNavigator({
+  Search: {
+    screen: SearchScreen,
+    navigationOptions: {
+      tabBarLabel: 'Search',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-search" size={22} />      
+      )
+    }
+  },
   Home: {
     screen: HomeScreen,
     navigationOptions: {
       tabBarIcon: ({tintColor}) => (
-        <Icon name="ios-home" size={24} />      
+        <Icon name="ios-home" size={22} />      
       )
     }
   },
-  AddGroup: {screen: AddGroup}
+  AddGroup: {
+    screen: AddGroup,
+    navigationOptions: {
+      tabBarLabel: 'Create Group',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name='ios-add' size={22} />
+      )
+    }
+  }
 }, {
     initialRouteName: 'Home'
 });
