7b0ed9c5c2413bfebd398a3e0c59ba3950e22c1a
Daniel McGrath <danmcgrath1035@gmail.com>
2019-05-18 12:20:56 -0400

    updated gradle and added styles for profile screen

--- app/controllers/profile/profileScreen.tsx
+++ app/controllers/profile/profileScreen.tsx
@@ -1,14 +1,51 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default class ProfileScreen extends Component {
     render() {
         return (
-            <View>
-                <Text>
-                    Profile Screen
-                </Text>
+            <View style={styles.container}>
+                <View style={styles.upperProfile}>
+                    <TouchableOpacity style={styles.fabButton}
+                        onPress= {() => {
+                           console.log('Hello world');
+                        }}
+                    >
+                        <View >
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.lowerProfile}>
+
+                </View>
+
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    upperProfile: {
+        flex: 1,
+        backgroundColor: 'lightgrey'
+    },
+    fabButton: {
+        height: 100,
+        width: 100,
+        borderRadius: 15,
+        borderColor: 'darkgrey',
+        borderWidth: 1,
+        backgroundColor: 'white',
+        position: 'absolute',
+        top: 80,
+        alignSelf: 'center',
+    },
+    lowerProfile: {
+        flex: 2,
+    }
+
+})
