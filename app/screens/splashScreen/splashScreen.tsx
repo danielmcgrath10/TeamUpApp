4cb335fa12b9ab2e57413e19e9a6da1ec9ee565f
LucasBriggs154 <lucas.briggs@gmail.com>
2020-04-05 16:26:46 -0600

    added functional navigation shell for splash animation

--- app/screens/splashScreen/splashScreen.tsx
+++ app/screens/splashScreen/splashScreen.tsx
@@ -3,11 +3,17 @@ import React, {Component} from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
 
 export default function SplashScreen({navigation}) {
+    setTimeout(() => {navigation.navigate('MainApp')}, 1000)
     return(
         <SafeAreaView style = {{flex: 1}}>
-            <Text>
-                Splash Screen
-            </Text>
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <Text style = {{justifyContent: 'center'}}>
+                    Splash Screen
+                </Text>
+            </View>
+
         </SafeAreaView>
+
+        
     )
 }
